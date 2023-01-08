71599405b20212b1d592b0546a427279b416ab09
SkyRanger05 <120250815+SkyRanger05@users.noreply.github.com>
2023-01-08 01:36:50 -0800

    New module 2 exercises

---
+++ Module2_copy_Class_1_Activities/01_Stu_Python_Code_Drills/Unsolved/07-functions-01/Unsolved/hello_functions-01_world.py
@@ -0,0 +1,30 @@
+# Define a function `having_fun` that prints "Functions are FUN!".
+def having_fun():
+    j = "Function are FUN!"
+    print(j)
+having_fun()
+
+# Define a function `thirty_seven` that prints the sum of 18 and 19.
+def thirty_seven(number1,number2):
+    h = number1 + number2
+    print(h)
+thirty_seven(18,19)
+
+# Call the two functions you've defined so far.
+having_fun()
+thirty_seven(9,0)
+
+# Define a function `hello` that takes in a string parameter and prints the parameter variable.
+def hello():
+    
+
+# Call your `hello` function.
+# YOUR CODE HERE!
+
+# Define a function `average` that calculates the average between two parameters and returns the average.
+# YOUR CODE HERE!
+
+# Call the `average` function and assign to a variable `calculated_average`.
+# Print your `calculated_average`.
+# YOUR CODE HERE!
+# YOUR CODE HERE!
